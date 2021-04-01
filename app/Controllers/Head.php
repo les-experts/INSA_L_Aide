a082a82632f2350944330ce812cf6790be0afd2b
Léo <leo.pacary@insa-rouen.fr>
2021-04-01 17:59:23 +0200

    ajt css sheet

--- app/Controllers/Head.php
+++ app/Controllers/Head.php
@@ -8,7 +8,8 @@ class Head extends BaseController
     public function getCss(){
 		helper('html');
         $link = array(
-			['href'  => base_url("static/css/style.css"),'rel'   => 'stylesheet','type'  => 'text/css'],
+			['href'  => base_url("static/css/FileTable.css"),'rel'   => 'stylesheet','type'  => 'text/css'],
+			['href'  => base_url("static/css/SearchBar.css"),'rel'   => 'stylesheet','type'  => 'text/css'],
 			['href'  => 'https://fonts.googleapis.com/icon?family=Material+Icons'],
 			['href'  => 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'],
 		);
