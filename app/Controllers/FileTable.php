ebbc9f8ea924b0adfd65d43949caf3ebcd5d5946
Léo <leo.pacary@insa-rouen.fr>
2021-04-01 18:17:44 +0200

    File table racine - Issue #8

--- app/Controllers/FileTable.php
+++ app/Controllers/FileTable.php
@@ -8,18 +8,11 @@ class FileTable extends BaseController
 	public function index()
 	{
 		helper('url'); 
-
-		if(func_num_args() <=0){
-			throw new Exception("Il n'y a aucun paramètres");
-		} 
 		$path = implode("/",func_get_args());
 		echo $this->view_dir($path);
 	}
 
 	public function view_dir($path){
-		if(empty($path)){
-			throw new Exception("Il n'y a aucun paramètres");
-		}
 
 		$model = new GitFileModel();
 		$files = $model->getFiles($path);
