eb2fe3256bfae6f3c428d8c110d6c33f37721548
avigneron <alexandre.vigneron@insa-rouen.fr>
2021-04-09 14:32:47 +0200

    Ajout de la page d'accueil. #4 #7

---
+++ app/Controllers/Presentation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+class Presentation extends BaseController
+{
+	public function index()
+	{
+		$path = implode("/",func_get_args());
+
+        $fileTable_object = new FileTable();
+        $fileTable_css = $fileTable_object->getCss();
+				$fileTable_html = $fileTable_object->view_dir($path,"Home/index");
+
+				$breadcrumb_object = new Breadcrumb();
+				$breadcrumb_css = $breadcrumb_object->getCss();
+				$breadcrumb_html = $breadcrumb_object->getView($path);
+
+        $data["cssLink"] = array_merge($fileTable_css, $breadcrumb_css);
+        $data["fileTable"] = $fileTable_html;
+				$data["breadcrumb"] = $breadcrumb_html;
+
+        return view("Presentation/index",$data);
+
+	}
+
+}
---
+++ app/Views/Presentation/index.php
@@ -0,0 +1,5 @@
+<?= $this->extend('Layouts/BaseLayout') ?>
+
+<?= $this->section('content') ?>
+    <h1>Page de présentation</h1>
+<?= $this->endSection() ?>
