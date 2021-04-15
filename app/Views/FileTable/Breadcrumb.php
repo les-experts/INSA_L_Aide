57c5067df7fb22961c9dd78ea8a008fe862eb6c4
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2021-04-15 13:15:04 +0200

    fixed breadcrumbs links

--- app/Views/FileTable/Breadcrumb.php
+++ app/Views/FileTable/Breadcrumb.php
@@ -6,7 +6,7 @@
          <?php
            foreach ($breadscrumbContentExploded as $key => $breadscrumbElement) {
                if ($key != count($breadscrumbContentExploded)-1 ) {
-                 echo "<a href='/index.php?folder=".explode("/".$breadscrumbContentExploded[$key+1],$pathToRepresent)[0]."' class='breadcrumb'>$breadscrumbElement</a>";
+                 echo "<a href='/Home/index/".explode("/".$breadscrumbContentExploded[$key+1],$pathToRepresent)[0]."' class='breadcrumb'>$breadscrumbElement</a>";
                } else {
                  echo "<a href='#!' class='breadcrumb'>$breadscrumbElement</a>";
                }
